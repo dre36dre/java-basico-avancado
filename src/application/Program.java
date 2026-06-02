b9aa457477a313a54334186be4b83ca9195ff78e
Anderson freires <andersonfreires1979@gmail.com>
2026-06-02 14:58:46 -0300

    findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
@@ -16,6 +17,12 @@ public static void main(String[] args) {
 	System.out.println("==================Test 1: findById ============");
 	Department department=departmentDao.findById(4);
 	System.out.println(department);
+	
+	System.out.println("==================Test 2: findAll =============");
+	List<Department> list= departmentDao.findAll();
+	for(Department d:list) {
+		System.out.println(d);
+	}
 	
 }
 }
